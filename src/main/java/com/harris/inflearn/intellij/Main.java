8fcb2f49239f1c8ab53f684af8ffd516d7420227
harris <harrisleesh@gmail.com>
2020-01-14 23:37:15 +0900

    IntelliJ Guide - Create Main Method and Sout

--- src/main/java/com/harris/inflearn/intellij/Main.java
+++ src/main/java/com/harris/inflearn/intellij/Main.java
@@ -1,7 +1,11 @@
 package com.harris.inflearn.intellij;
 
 public class Main {
+    //Main Method 만들기 Code Template psvm
     public static void main(String[] args) {
-        System.out.println("aaa");
+        // 표준 출력 sout
+        System.out.println("표준 출력 테스트");
+
+        //실행 ctrl + shift + F10
     }
 }
--- src/main/java/com/harris/inflearn/intellij/Main2.java
+++ src/main/java/com/harris/inflearn/intellij/Main2.java
@@ -2,6 +2,9 @@ package com.harris.inflearn.intellij;
 
 public class Main2 {
     public static void main(String[] args) {
-        System.out.println("bbb");
+        //실행 ctrl + shift + F10
+        System.out.println("표준 출력 테스트");
+
+        // 방금 실행시켰던 환경을 다시 실행 shift + F10
     }
 }
