7bff1580216facbfae94bda3bc6953be7b80c8fb
harris <harrisleesh@gmail.com>
2020-01-22 21:43:32 +0900

    Text find and replace with regex
    ctrl + r
    ctrl + f
    ctrl + shift + r
    ctrl + shift + f
    and REGEX

---
+++ src/main/java/com/harris/inflearn/intellij/chap3/text/FindAndReplace.java
@@ -0,0 +1,16 @@
+package com.harris.inflearn.intellij.chap3.text;
+
+public class FindAndReplace {
+
+    public void find(){
+        //ctrl + f
+        String str = "textFind";
+    }
+
+    public void replace(){
+        //ctrl + r
+        String str = "textReplace";
+
+        System.out.println("textReplace");
+    }
+}
---
+++ src/main/java/com/harris/inflearn/intellij/chap3/text/ProjectFindAndReplace.java
@@ -0,0 +1,13 @@
+package com.harris.inflearn.intellij.chap3.text;
+
+public class ProjectFindAndReplace {
+    public void projectFind(){
+        //ctrl + shift + f
+        System.out.println("전체탐색 : projectFind");
+    }
+
+    public void replace(){
+        //ctrl + shift + r
+        System.out.println("ProjectFindAndReplace 에서 텍스트 교체 : projectReplace");
+    }
+}
